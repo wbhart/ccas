9bb02787f1d4a6c23415f80376c45f98529a3b55
William Hart <goodwillhart@googlemail.com>
2015-02-22 20:43:56 +0000

    Add special cases for pow.

--- Poly/pow.c
+++ Poly/pow.c
@@ -6,35 +6,44 @@ void poly_pow(Poly_t a, Poly_t b, int_t n)
    Poly_t temp;
    Poly_struct * r = a;
 
-   if (a == b)
+   if (n == 0)
+      poly_seti(a, 1);
+   else if (b->length == 0)
+      a->length = 0;
+   else if (n == 1)
+      poly_set(a, b);
+   else
    {
-      poly_init(temp, a->parent);
-      r = temp;
-   }
+      if (a == b)
+      {
+         poly_init(temp, a->parent);
+         r = temp;
+      }
 
-   uint_t bit = ~((~(uint_t) 0) >> 1);
+      uint_t bit = ~((~(uint_t) 0) >> 1);
    
-   while (((uint_t) bit & n) == 0)
+      while ((bit & n) == 0)
+         bit >>= 1;
+
+      poly_set(r, b);
+
       bit >>= 1;
 
-   poly_set(r, b);
+      while (bit != 0)
+      {
+         poly_mul(r, r, r);
 
-   bit >>= 1;
-
-   while (bit != 0)
-   {
-      poly_mul(r, r, r);
-
-      if (((uint_t) bit & n) != 0)
-         poly_mul(r, r, b);
+         if ((bit & n) != 0)
+            poly_mul(r, r, b);
        
-      bit >>= 1;
-   }
+         bit >>= 1;
+      }
 
-   if (a == b)
-   {
-      poly_swap(a, r);
-      poly_clear(temp);
+      if (a == b)
+      {
+         poly_swap(a, r);
+         poly_clear(temp);
+      }
    }
 }
 
